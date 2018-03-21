b53dd1ac515f52acbe6784c2f9bc4af82a61357a
Gryphon Myers <gryphon.myers@2k.com>
2018-03-21 14:29:35 -0700

    fix bug causing non-plain objects to get replaced

--- src/make-defaults-func.js
+++ src/make-defaults-func.js
@@ -4,7 +4,7 @@ module.exports = function(deep, merge) {
             Object.entries(obj).forEach((entry) => {
                 if (typeof sourceObj[entry[0]] === 'undefined') {
                     sourceObj[entry[0]] = entry[1];
-                } else if (deep && [entry[1], sourceObj[entry[0]]].every(val => val && typeof val === 'object' && !Array.isArray(val))) {
+                } else if (deep && [entry[1], sourceObj[entry[0]]].every(val => val && typeof val === 'object' && val.constructor.name === 'Object')) {
                     sourceObj[entry[0]] = defaults(sourceObj[entry[0]], entry[1]);
                 } else if (merge && [entry[1], sourceObj[entry[0]]].every(val => val && typeof val === 'object' && Array.isArray(val))) {
                     sourceObj[entry[0]] = merge(sourceObj[entry[0]], entry[1]);
